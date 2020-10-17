e7aa7f0a445bcf6fcafe48f75499c95458930d38
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-10-17 19:32:33 +0545

    finished phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import SearchFilter from "./Components/SearchFilter";
 import Person from "./Components/Person";
 import PersonForm from "./Components/PersonForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((resp) => {
+      setPersons(resp.data);
+    });
+  }, []);
 
   const handleNewFilter = (event) => {
     setFilter(event.target.value);
