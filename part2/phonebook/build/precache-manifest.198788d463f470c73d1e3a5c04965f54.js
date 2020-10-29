588e96db25e0251a50beb31a894dcc94de5d98bc
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-10-29 20:48:00 +0545

    made part2 fullstack compatible

---
+++ part2/phonebook/build/precache-manifest.198788d463f470c73d1e3a5c04965f54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e79222f6ce8780e6e1fd4ef3d1cc673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35888f3a060fff57d34d",
+    "url": "/static/css/main.b5adea53.chunk.css"
+  },
+  {
+    "revision": "1c4266a9675edd20d256",
+    "url": "/static/js/2.cd9ad85c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cd9ad85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35888f3a060fff57d34d",
+    "url": "/static/js/main.91115af0.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
