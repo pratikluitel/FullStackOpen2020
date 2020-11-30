836919607a5bb02514a726c6742261f9a976943b
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-11-30 11:20:41 +0545

    finished anecdotes

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { voteAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return anecdotes
+    .sort((a, b) => (a.votes > b.votes ? -1 : 1))
+    .map((anecdote) => (
+      <div key={anecdote.id}>
+        <div>{anecdote.content}</div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>
+            vote
+          </button>
+        </div>
+      </div>
+    ));
+};
+
+export default AnecdoteList;
